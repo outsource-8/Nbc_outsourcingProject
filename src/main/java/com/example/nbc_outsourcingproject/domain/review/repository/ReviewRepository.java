197ab5f24334b151ff697e11cd5f4992dfc896d4
younghwan314 <younghwan314@naver.com>
2025-02-28 21:03:11 +0900

    Core: Review 구조 생성

---
+++ src/main/java/com/example/nbc_outsourcingproject/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,4 @@
+package com.example.nbc_outsourcingproject.domain.review.repository;
+
+public interface ReviewRepository {
+}
---
+++ src/main/java/com/example/nbc_outsourcingproject/domain/review/service/ReviewService.java
@@ -0,0 +1,4 @@
+package com.example.nbc_outsourcingproject.domain.review.service;
+
+public class ReviewService {
+}
