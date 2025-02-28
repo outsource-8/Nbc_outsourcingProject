7a833e5d84dbc8134756767f8cad2caf21bc9502
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-02-28 20:36:54 +0900

    first commit

---
+++ src/test/java/com/example/nbc_outsourcingproject/NbcOutsourcingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbc_outsourcingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcOutsourcingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
