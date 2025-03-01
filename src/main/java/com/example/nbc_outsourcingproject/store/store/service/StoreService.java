512fe1f66b8fb6f341d9d2c8f84c1025dea3f6c7
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-01 22:06:27 +0900

    Feat: store 다건 조회

---
+++ src/main/java/com/example/nbc_outsourcingproject/store/store/service/StoreService.java
@@ -0,0 +1,33 @@
+package com.example.nbc_outsourcingproject.store.store.service;
+
+import com.example.nbc_outsourcingproject.store.store.dto.response.StoreResponse;
+import com.example.nbc_outsourcingproject.store.store.entity.Store;
+import com.example.nbc_outsourcingproject.store.store.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    // 가게 다건 조회
+    public Page<StoreResponse> getStores(String storeName, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Store> stores = storeRepository.findStores(storeName, pageable);
+        return stores.map(store -> new StoreResponse(
+                store.getName(),
+                store.getStoreInfo(),
+                store.getMinOrderAmount(),
+                store.getClosed(),
+                store.getOpened()
+        ));
+    }
+}
