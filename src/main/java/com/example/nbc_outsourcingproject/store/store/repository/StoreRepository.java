205c5ce56be9f4ef7c24a34c1f565e0d351ea82d
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-01 22:06:27 +0900

    Feat: store 다건 조회

--- src/main/java/com/example/nbc_outsourcingproject/store/store/repository/StoreRepository.java
+++ src/main/java/com/example/nbc_outsourcingproject/store/store/repository/StoreRepository.java
@@ -1,8 +1,20 @@
 package com.example.nbc_outsourcingproject.store.store.repository;
 
 import com.example.nbc_outsourcingproject.store.store.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
+    // 가게 다건 조회
+    @Query("SELECT s FROM Store s JOIN FETCH s.fakeUser WHERE " +
+            "(:name IS NULL OR s.name LIKE CONCAT('%', :name, '%') ) "+
+            "ORDER BY s.name")
+    Page<Store> findStores(
+            @Param("name") String name,
+            Pageable pageable
+    );
 }
