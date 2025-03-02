af4ec2ef9225ad024c1254e830f97d1ec896f3d5
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-02 15:35:53 +0900

    Feat: 소유한 가게 조회

--- src/main/java/com/example/nbc_outsourcingproject/store/store/controller/StoreOwnerController.java
+++ src/main/java/com/example/nbc_outsourcingproject/store/store/controller/StoreOwnerController.java
@@ -1,5 +1,6 @@
 package com.example.nbc_outsourcingproject.store.store.controller;
 
+import com.example.nbc_outsourcingproject.store.store.dto.response.StoreResponse;
 import com.example.nbc_outsourcingproject.store.store.entity.FakeUser;
 import com.example.nbc_outsourcingproject.store.store.dto.request.StoreSaveRequest;
 import com.example.nbc_outsourcingproject.store.store.dto.response.StoreSaveResponse;
@@ -7,9 +8,9 @@ import com.example.nbc_outsourcingproject.store.store.service.StoreOwnerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +18,20 @@ public class StoreOwnerController {
 
     private final StoreOwnerService storeOwnerService;
 
+    // 가게 생성
     @PostMapping("/stores")
     public ResponseEntity<StoreSaveResponse> createStore(
             FakeUser fakeUser,
             @Valid @RequestBody StoreSaveRequest storeSaveRequest
     ) {
         return ResponseEntity.ok(storeOwnerService.saveStore(fakeUser, storeSaveRequest));
+    }
+
+    // 소유한 가게 조회
+    @GetMapping("/stores/mine")
+    public ResponseEntity<List<StoreResponse>> getStoresMine(
+            @RequestParam() Long fakeUserId
+    ){
+        return ResponseEntity.ok(storeOwnerService.getStoresMine(fakeUserId));
     }
 }
--- src/main/java/com/example/nbc_outsourcingproject/store/store/service/StoreOwnerService.java
+++ src/main/java/com/example/nbc_outsourcingproject/store/store/service/StoreOwnerService.java
@@ -1,6 +1,7 @@
 package com.example.nbc_outsourcingproject.store.store.service;
 
 import com.example.nbc_outsourcingproject.store.common.exception.UnauthorizedException;
+import com.example.nbc_outsourcingproject.store.store.dto.response.StoreResponse;
 import com.example.nbc_outsourcingproject.store.store.entity.FakeUser;
 import com.example.nbc_outsourcingproject.store.store.entity.Store;
 import com.example.nbc_outsourcingproject.store.store.dto.request.StoreSaveRequest;
@@ -10,6 +11,8 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -44,10 +47,28 @@ public class StoreOwnerService {
         );
     }
 
+    // 소유한 가게 조회
+    public List<StoreResponse> getStoresMine(Long fakeUserId) {
+        // validateOwner(fakeUser);
+        // 사용자 아이디 가져오기
+        // Long fakeUserId = fakeUser.getId();
+
+        List<Store> storesMine = storeRepository.findAllByFakeUserId(fakeUserId);
+
+        return storesMine.stream()
+                .map(store -> new StoreResponse(
+                        store.getName(),
+                        store.getStoreInfo(),
+                        store.getMinOrderAmount(),
+                        store.getClosed(),
+                        store.getOpened()))
+                .toList();
+    }
+
     // OWNER 사용자 검증
     private void validateOwner(FakeUser fakeUser) {
         if (!fakeUser.isOwner()) {
-            throw new UnauthorizedException("Owner만 가게를 생성할 수 있습니다.");
+            throw new UnauthorizedException("Owner가 아닙니다.");
         }
     }
 }
