f0bf0d65bfc45b15a646ef9f58b2e7b53fd087aa
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-02 15:24:09 +0900

    Test : 가게 전체 조회 테스트

--- src/test/java/com/example/nbc_outsourcingproject/store/StoreServiceTest.java
+++ src/test/java/com/example/nbc_outsourcingproject/store/StoreServiceTest.java
@@ -31,10 +31,38 @@ public class StoreServiceTest {
     private StoreRepository storeRepository;
 
     @Test
-    public void 가게를_다_건_조회한다() {
+    public void 이름을_입력하여_가게를_다_건_조회한다() {
         //given
         FakeUser fakeUser = new FakeUser(1L, "사용자", "OWNER");
-        String storeName = "1";
+        String storeName = "test";
+        int page = 1;
+        int size = 10;
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        List<Store> storeList = List.of(
+                new Store(fakeUser, "test1", "address2", 2000, "storeInfo2", LocalTime.of(15, 30, 45), LocalTime.of(14, 30, 45)),
+                new Store(fakeUser, "test2", "address2", 2000, "storeInfo2", LocalTime.of(18, 30, 45), LocalTime.of(14, 30, 45))
+        );
+
+        Page<Store> storesPage = new PageImpl<>(storeList, pageable, storeList.size());
+        given(storeRepository.findStores(storeName, pageable)).willReturn(storesPage);
+
+        //when
+        Page<StoreResponse> response = storeService.getStores(storeName, page, size);
+
+        //then
+        assertNotNull(response);
+        assertEquals(storeList.size(), response.getContent().size());
+
+        for (StoreResponse storeResponse : response.getContent()) {
+            assertTrue(storeResponse.getName().contains(storeName));  // storeName이 포함된 가게만 반환 되었는지 확인
+        }
+    }
+
+    @Test
+    public void 이름을_입력하지_않으면_가게를_전체_조회한다() {
+        //given
+        FakeUser fakeUser = new FakeUser(1L, "사용자", "OWNER");
         int page = 1;
         int size = 10;
         Pageable pageable = PageRequest.of(page - 1, size);
@@ -45,21 +73,18 @@ public class StoreServiceTest {
                 new Store(fakeUser, "test2", "address2", 2000, "storeInfo2", LocalTime.of(18, 30, 45), LocalTime.of(14, 30, 45))
         );
 
-        List<Store> filteredList = storeList.stream().filter(store -> store.getName().contains(storeName)).toList();
-        Page<Store> storesPage = new PageImpl<>(filteredList, pageable, storeList.size());
-        given(storeRepository.findStores(storeName, pageable)).willReturn(storesPage);
+        Page<Store> storesPage = new PageImpl<>(storeList, pageable, storeList.size());
+        given(storeRepository.findStores(null, pageable)).willReturn(storesPage);
 
         //when
-        Page<StoreResponse> response = storeService.getStores(storeName, page, size);
+        Page<StoreResponse> response = storeService.getStores(null, page, size);
 
         //then
         assertNotNull(response);
-        assertEquals(filteredList.size(),response.getContent().size());
-
-        for (StoreResponse storeResponse : response.getContent()) {
-            assertTrue(storeResponse.getName().contains(storeName));  // storeName이 포함된 가게만 반환 되었는지 확인
-        }
-
+        assertEquals(storeList.size(), response.getContent().size());
+        List<String> storeNames = response.stream().map(StoreResponse::getName).toList();
+        assertTrue(storeNames.contains("name1"));
+        assertTrue(storeNames.contains("test1"));
+        assertTrue(storeNames.contains("test2"));
     }
-
 }
