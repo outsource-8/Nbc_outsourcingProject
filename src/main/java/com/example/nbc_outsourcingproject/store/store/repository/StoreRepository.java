ae41368724246cdd0d426d33208a57781de9013a
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-02 15:35:53 +0900

    Feat: 소유한 가게 조회

--- src/main/java/com/example/nbc_outsourcingproject/store/store/repository/StoreRepository.java
+++ src/main/java/com/example/nbc_outsourcingproject/store/store/repository/StoreRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     // 가게 다건 조회
@@ -17,4 +19,8 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
             @Param("name") String name,
             Pageable pageable
     );
+
+    // 소유한 가게 다건 조회
+    @Query("SELECT s From Store s JOIN FETCH s.fakeUser WHERE s.fakeUser.Id = :userId")
+    List<Store> findAllByFakeUserId(@Param("userId") Long userId);
 }
