7584e06153a3991abd6080c75469ee72b297cd2d
euuns <dmsu2@naver.com>
2025-03-03 21:02:46 +0900

    Fix(menu): MenuSerivce 오타 수정

--- src/main/java/com/example/nbc_outsourcingproject/domain/menu/controller/MenuController.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/menu/controller/MenuController.java
@@ -3,7 +3,7 @@ package com.example.nbc_outsourcingproject.domain.menu.controller;
 import com.example.nbc_outsourcingproject.domain.menu.dto.MenuRequest;
 import com.example.nbc_outsourcingproject.domain.menu.dto.MenuResponse;
 import com.example.nbc_outsourcingproject.domain.menu.dto.MenuUpdateRequest;
-import com.example.nbc_outsourcingproject.domain.menu.service.MenuSerivce;
+import com.example.nbc_outsourcingproject.domain.menu.service.MenuService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,12 +17,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MenuController {
 
-    private final MenuSerivce menuSerivce;
+    private final MenuService menuService;
 
     @PostMapping("/{storeId}/menus")
     public ResponseEntity<String> createMenu(@PathVariable Long storeId, @Auth AuthUser authUser,
                                              @Valid @RequestBody MenuRequest menuRequest) {
-        menuSerivce.createMenu(storeId, authUser.getId(), menuRequest.getCategory(), menuRequest.getName(), menuRequest.getPrice(), menuRequest.getInfo());
+        menuService.createMenu(storeId, authUser.getId(), menuRequest.getCategory(), menuRequest.getName(), menuRequest.getPrice(), menuRequest.getInfo());
         return new ResponseEntity<>("등록에 성공하였습니다.", HttpStatus.CREATED);
     }
 
@@ -31,19 +31,19 @@ public class MenuController {
                                                    @Auth AuthUser authUser,
                                                    @PathVariable Long menuId,
                                                    @Valid @RequestBody MenuUpdateRequest menuRequest) {
-        MenuResponse menuResponse = menuSerivce.updateMenu(storeId, authUser.getId(), menuId, menuRequest.getCategory(), menuRequest.getName(), menuRequest.getPrice(), menuRequest.getInfo());
+        MenuResponse menuResponse = menuService.updateMenu(storeId, authUser.getId(), menuId, menuRequest.getCategory(), menuRequest.getName(), menuRequest.getPrice(), menuRequest.getInfo());
         return new ResponseEntity<>(menuResponse, HttpStatus.OK);
     }
 
     @GetMapping("/{storeId}/menus")
     public ResponseEntity<List<MenuResponse>> getMenus(@PathVariable Long storeId, @Auth AuthUser authUser, @RequestParam(required = false) Long menuId) {
-        List<MenuResponse> menus = menuSerivce.getMenus(storeId, authUser.getId(), menuId);
+        List<MenuResponse> menus = menuService.getMenus(storeId, authUser.getId(), menuId);
         return new ResponseEntity<>(menus, HttpStatus.OK);
     }
 
     @DeleteMapping("/{storeId}/menus/{menuId}")
     public ResponseEntity<String> deleteMenu(@PathVariable Long storeId, @Auth AuthUser authUser, @PathVariable Long menuId) {
-        menuSerivce.deleteMenu(storeId, authUser.getId(), menuId);
+        menuService.deleteMenu(storeId, authUser.getId(), menuId);
         return new ResponseEntity<>("메뉴를 삭제하였습니다.", HttpStatus.OK);
     }
 }
