18dc898b4367d920a17dfcb38f6f2818191c87eb
euuns <dmsu2@naver.com>
2025-03-03 14:50:26 +0900

    Feat(menu): Create menu entity

---
+++ src/main/java/com/example/nbc_outsourcingproject/domain/menu/entity/Category.java
@@ -0,0 +1,11 @@
+package com.example.nbc_outsourcingproject.domain.menu.entity;
+
+public enum Category {
+    MAIN,
+    SINGLE,
+    SET,
+    SIDE,
+    COFFEE,
+    BEVERAGE,
+    DESSERT;
+}
--- src/main/java/com/example/nbc_outsourcingproject/domain/menu/entity/Menu.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/menu/entity/Menu.java
@@ -1,4 +1,40 @@
 package com.example.nbc_outsourcingproject.domain.menu.entity;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity @Getter
+@Table( uniqueConstraints = {
+        @UniqueConstraint(
+                name = "unique_store_name",
+                columnNames = {"store_id", "name"})
+})
 public class Menu {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    private String info;
+
+    private boolean isDeleted;
+
+
+    public Menu(){}
+
+
 }
