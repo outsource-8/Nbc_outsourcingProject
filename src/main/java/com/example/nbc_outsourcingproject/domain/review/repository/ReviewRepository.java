9d7b5acaaf01f60743961c42f946ed1ca4033e59
younghwan314 <younghwan314@naver.com>
2025-03-04 20:17:25 +0900

    feat: review 다건조회

--- src/main/java/com/example/nbc_outsourcingproject/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.example.nbc_outsourcingproject.domain.review.repository;
 
 import com.example.nbc_outsourcingproject.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByStoreId(Long storeId, Pageable pageable);
 }
