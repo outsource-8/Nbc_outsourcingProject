dac60df9899ea4d7a99087682868a0a661289943
younghwan314 <younghwan314@naver.com>
2025-03-04 19:27:44 +0900

    fix: create 변수명 수정

--- src/main/java/com/example/nbc_outsourcingproject/domain/review/dto/response/CreateReviewResponse.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/review/dto/response/CreateReviewResponse.java
@@ -9,24 +9,24 @@ public class CreateReviewResponse {
     private final Long id;
     private final int rating;
     private final String content;
-    private final Long order_id;
-    private final Long user_id;
-    private final Long store_id;
+    private final Long orderId;
+    private final Long userId;
+    private final Long storeId;
 
     @Builder
     public CreateReviewResponse(
             Long id,
             int rating,
             String content,
-            Long order_id,
-            Long user_id,
-            Long store_id
+            Long orderId,
+            Long userId,
+            Long storeId
     ) {
         this.id = id;
         this.rating = rating;
         this.content = content;
-        this.order_id = order_id;
-        this.user_id = user_id;
-        this.store_id = store_id;
+        this.orderId = orderId;
+        this.userId = userId;
+        this.storeId = storeId;
     }
 }
