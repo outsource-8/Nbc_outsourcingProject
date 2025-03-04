4dae6c8bedcb2bb4f589a52d29491522dc48db53
euuns <dmsu2@naver.com>
2025-03-04 19:41:07 +0900

    Refactor(common): exception관리 클래스 위치 변경(menu->common)

--- src/main/java/com/example/nbc_outsourcingproject/domain/common/exception/ExceptionHandler.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/common/exception/ExceptionHandler.java
@@ -1,13 +1,13 @@
-package com.example.nbc_outsourcingproject.domain.menu.exception;
+package com.example.nbc_outsourcingproject.domain.common.exception;
 
+import com.example.nbc_outsourcingproject.domain.menu.exception.MenuException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class MenuExceptionHandler {
+public class ExceptionHandler {
 
-    @ExceptionHandler(MenuException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MenuException.class)
     public ResponseEntity<ErrorResponse> handleMenuException(MenuException e) {
         return new ResponseEntity<>(ErrorResponse.of(e.getMenuExceptionCode().name(), e.getHttpStatus(), e.getMessage()), e.getHttpStatus());
     }
