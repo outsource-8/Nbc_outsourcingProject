9e5d16a8464e496b446cae7a5c4ee806a28fd5b6
younghwan314 <younghwan314@naver.com>
2025-03-04 14:04:21 +0900

    feat: review

--- src/main/java/com/example/nbc_outsourcingproject/domain/review/entity/Review.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/review/entity/Review.java
@@ -1,4 +1,24 @@
 package com.example.nbc_outsourcingproject.domain.review.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
 public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(nullable = false)
+    private String content;
+
+    public Review() {
+
+    }
 }
