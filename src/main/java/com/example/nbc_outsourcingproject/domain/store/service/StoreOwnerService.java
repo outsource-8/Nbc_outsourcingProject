ed55a63fcf769da09298fe57f0c17b54892c1f86
euuns <dmsu2@naver.com>
2025-03-05 13:42:02 +0900

    Feat: StoreOwnerService.saveStore 에서 캐시 저장 제외(임시)

--- src/main/java/com/example/nbc_outsourcingproject/domain/store/service/StoreOwnerService.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/store/service/StoreOwnerService.java
@@ -45,7 +45,6 @@ public class StoreOwnerService {
         );
 
         Store savedStore = storeRepository.save(store);
-        saveStoreToCache(user.getId(), savedStore.getId()); //생성된 가게 캐시에 저장
 
         return new StoreSaveResponse(
                 savedStore.getName(),
