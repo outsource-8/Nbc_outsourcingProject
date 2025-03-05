19851883b2e592c9c4957ccf2a7f51f19c2643b1
codingTrip <codingtrip@naver.com>
2025-03-05 15:45:14 +0900

    Fixing: option 여러 개 받는 부분 수정중

--- src/main/java/com/example/nbc_outsourcingproject/domain/order/dto/OrderMenuResponse.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/order/dto/OrderMenuResponse.java
@@ -1,7 +1,10 @@
 package com.example.nbc_outsourcingproject.domain.order.dto;
 
+import com.example.nbc_outsourcingproject.domain.menuoption.dto.MenuOptionResponse;
 import com.example.nbc_outsourcingproject.domain.order.entity.OrderMenu;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class OrderMenuResponse {
@@ -9,16 +12,14 @@ public class OrderMenuResponse {
     private final String menuName;
     private final int menuPrice;
     private final int quantity;
-    private final String optionName;
-    private final int optionPrice;
+    private final List<MenuOptionResponse> menuOptionResponses;
 
     public OrderMenuResponse(OrderMenu orderMenu) {
         this.id = orderMenu.getId();
         this.menuName = orderMenu.getCurrentMenuName();
         this.menuPrice = orderMenu.getCurrentMenuPrice();  // 메뉴 이름 포함
         this.quantity = orderMenu.getQuantity();
-        this.optionName = orderMenu.getCurrentOptionName();
-        this.optionPrice = orderMenu.getCurrentOptionPrice();
+        this.menuOptionResponses = orderMenu.getMenuOptions().stream().map(MenuOptionResponse::from).toList();
     }
 }
 
