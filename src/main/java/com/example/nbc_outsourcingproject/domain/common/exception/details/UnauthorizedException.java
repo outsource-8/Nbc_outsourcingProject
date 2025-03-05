4468c9c53050cd38662d76d9070179c9fb3437fb
euuns <dmsu2@naver.com>
2025-03-05 12:14:36 +0900

    Refactor: exception 파일 정리

--- src/main/java/com/example/nbc_outsourcingproject/domain/common/exception/details/UnauthorizedException.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/common/exception/details/UnauthorizedException.java
@@ -1,4 +1,4 @@
-package com.example.nbc_outsourcingproject.domain.common.exception;
+package com.example.nbc_outsourcingproject.domain.common.exception.details;
 
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException(String message) {
