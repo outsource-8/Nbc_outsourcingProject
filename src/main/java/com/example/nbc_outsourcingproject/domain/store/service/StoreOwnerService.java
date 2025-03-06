f41ab14cb557951ab6236e8f7baede95d97ad98d
euuns <dmsu2@naver.com>
2025-03-07 04:20:20 +0900

    Impl: 가게 영업 시간 조회 및 예외 처리 추가

--- src/main/java/com/example/nbc_outsourcingproject/domain/store/service/StoreOwnerService.java
+++ src/main/java/com/example/nbc_outsourcingproject/domain/store/service/StoreOwnerService.java
@@ -17,6 +17,7 @@ import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
